6ef1a32ccb36bea80ec3073cb2d1d527825c231b
maxint <lnychina@gmail.com>
2015-06-20 14:18:43 +0800

    BinaryMapExp

--- main.cpp
+++ main.cpp
@@ -11,18 +11,23 @@ struct Exp {
     }
 };
 
-// addition operation
 template<typename TLhs, typename TRhs>
-struct BinaryAddExp : Exp<BinaryAddExp<TLhs, TRhs> > {
+struct ReturnType {
+    typedef typename TLhs::value_type value_type;
+};
+
+// general binary operation
+template<typename OP, typename TLhs, typename TRhs>
+struct BinaryMapExp : Exp<BinaryMapExp<OP, TLhs, TRhs> > {
     const TLhs &lhs;
     const TRhs &rhs;
-    typedef typename TLhs::value_type value_type;
+    typedef typename ReturnType<TLhs, TRhs>::value_type value_type;
 
-    BinaryAddExp(const TLhs &lhs, const TRhs &rhs)
+    BinaryMapExp(const TLhs &lhs, const TRhs &rhs)
         : lhs(lhs), rhs(rhs) {}
 
     inline value_type Eval(int i) const {
-        return lhs.Eval(i) + rhs.Eval(i);
+        return OP::Map(lhs.Eval(i), rhs.Eval(i));
     }
 };
 
@@ -51,10 +56,75 @@ struct Vec : public Exp<Vec<T> > {
     }
 };
 
-// do addition
+// template works for any expressions
+template<typename OP, typename TLhs, typename TRhs>
+inline BinaryMapExp<OP, TLhs, TRhs>
+F(const Exp<TLhs> &lhs, const Exp<TRhs> &rhs) {
+    return BinaryMapExp<OP, TLhs, TRhs>(lhs.self(), rhs.self());
+}
+
+namespace detail {
+
+template<typename T> struct add {
+    inline static T Map(const T& a, const T& b) {
+        return a + b;
+    }
+};
+
+template<typename T> struct sub {
+    inline static T Map(const T& a, const T& b) {
+        return a - b;
+    }
+};
+
+template<typename T> struct mul {
+    inline static T Map(const T& a, const T& b) {
+        return a * b;
+    }
+};
+
+template<typename T> struct div {
+    inline static T Map(const T& a, const T& b) {
+        return a / b;
+    }
+};
+
+template<typename T> struct maximum {
+    inline static T Map(const T& a, const T& b) {
+        return a > b ? a : b;
+    }
+};
+
+template<typename T> struct minimum {
+    inline static T Map(const T& a, const T& b) {
+        return a < b ? a : b;
+    }
+};
+
+} // namespace detail
+
 template<typename TLhs, typename TRhs>
-BinaryAddExp<TLhs, TRhs> operator+(const Exp<TLhs> &lhs, const Exp<TRhs> &rhs) {
-    return BinaryAddExp<TLhs, TRhs>(lhs.self(), rhs.self());
+inline BinaryMapExp<detail::add<typename ReturnType<TLhs, TRhs>::value_type>, TLhs, TRhs>
+operator+(const Exp<TLhs> &lhs, const Exp<TRhs> &rhs) {
+    return F<detail::add<typename ReturnType<TLhs, TRhs>::value_type> >(lhs, rhs);
+}
+
+template<typename TLhs, typename TRhs>
+inline BinaryMapExp<detail::sub<typename ReturnType<TLhs, TRhs>::value_type>, TLhs, TRhs>
+operator-(const Exp<TLhs> &lhs, const Exp<TRhs> &rhs) {
+    return F<detail::sub<typename ReturnType<TLhs, TRhs>::value_type> >(lhs, rhs);
+}
+
+template<typename TLhs, typename TRhs>
+inline BinaryMapExp<detail::mul<typename ReturnType<TLhs, TRhs>::value_type>, TLhs, TRhs>
+operator*(const Exp<TLhs> &lhs, const Exp<TRhs> &rhs) {
+    return F<detail::mul<typename ReturnType<TLhs, TRhs>::value_type> >(lhs, rhs);
+}
+
+template<typename TLhs, typename TRhs>
+inline BinaryMapExp<detail::div<typename ReturnType<TLhs, TRhs>::value_type>, TLhs, TRhs>
+operator/(const Exp<TLhs> &lhs, const Exp<TRhs> &rhs) {
+    return F<detail::div<typename ReturnType<TLhs, TRhs>::value_type> >(lhs, rhs);
 }
 
 int main() {
@@ -68,6 +138,11 @@ int main() {
     for (int i = 0; i < n; ++i) {
         printf("%d:%f == %f + %f + %f\n", i,
                A.dptr[i], B.dptr[i], C.dptr[i], C.dptr[i]);
+    }
+    A = B * C + F<detail::maximum<float> >(B, C);
+    for (int i = 0; i < n; ++i) {
+        printf("%d:%f == %f * %f + max(%f, %f)\n", i,
+               A.dptr[i], B.dptr[i], C.dptr[i], B.dptr[i], C.dptr[i]);
     }
     return 0;
 }
